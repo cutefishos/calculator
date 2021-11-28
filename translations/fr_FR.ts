e260aa6ebc4826d33b226c8a84e2a03944791794
Roxfr <52124613+roxfr@users.noreply.github.com>
2021-11-28 10:18:27 +0100

    Create fr_FR.ts

---
+++ translations/fr_FR.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1"  language="fr_FR">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="14"/>
+        <source>Calculator</source>
+        <translation>Calculatrice</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="25"/>
+        <source>Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="28"/>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="37"/>
+        <source>A simple calculator</source>
+        <translation>Une simple calculatrice</translation>
+    </message>
+</context>
+</TS>
